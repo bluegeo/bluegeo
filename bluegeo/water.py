3fe3f8c689c7a9996c4fdf86608108ca0eed539d
Devin Cairns <devin.cairns@bluegeosimulation.com>
2018-02-16 10:42:42 -0800

    sort basins by area in delineate_watershed

--- bluegeo/water.py
+++ bluegeo/water.py
@@ -52,6 +52,10 @@ def delineate_watersheds(points, dem=None, fd=None, fa=None, as_vector=True, sna
 
     basins = [bluegrass.water_outlet([point], direction=fd).polygonize()[:][0] for point in points]
 
+    # Sort basins by area (largest to smallest)
+    srt = numpy.argsort([ogr.CreateGeometryFromWkb(b).Area() for b in basins])[::-1]
+    basins = [basins[i] for i in srt]
+
     return Vector(basins, mode='w', projection=Raster(fd).projection)
 
 
